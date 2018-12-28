a62f3de05a6612049a5ac49f6d4fcff3a7c71e45
Joseph Ward <jward@josephward.com>
2018-12-27 17:50:49 -0700

    updated config

--- config.js
+++ config.js
@@ -13,5 +13,5 @@ module.exports = {
     // linkedin: 'https://google.com',
   },
   pathPrefix: '/devblog',
-  // siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+  siteUrl: 'https://serverside.io/',
 };
